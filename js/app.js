e4e00b072b0d075f91ac7cfffb11b3dc5c5d8c73
Ganz <loganbellinghamlb@gmail.com>
2021-12-02 01:33:09 +1300

    Basis for drawing pad grid generation complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
+// -- ELEMENTS -- //
 const drawingPadContainer = document.querySelector(".drawing-pad-container");
-const pixelsPerRow = 54;
-const totalPixels = pixelsPerRow * pixelsPerRow;
-const height = 500 / pixelsPerRow
+const input = document.querySelector("input");
 
-for (let i = 0; i < totalPixels; i++) {
-    const div = document.createElement("div");
-    div.classList.add("drawing-pad-pixel");
-    div.setAttribute("style", `height: ${height}px; width: ${height}px`);
-    drawingPadContainer.appendChild(div);
-}
+// -- VARIABLES -- //
+let initialSquaresPerRow = 5;
+let totalSquares = initialSquaresPerRow * initialSquaresPerRow;
+
+
+
+const createGrid = (squaresPerRow = initialSquaresPerRow, totalSquareAmount = totalSquares) => {
+    for (let i = 0; i < totalSquareAmount; i++) {
+        const div = document.createElement("div");
+        div.classList.add("drawing-pad-square");
+       
+        drawingPadContainer.style.gridTemplateRows = `repeat(${squaresPerRow}, 1fr)`;
+        drawingPadContainer.style.gridTemplateColumns = `repeat(${squaresPerRow}, 1fr)`;
+    
+        drawingPadContainer.appendChild(div);
+    }
+};
+
+// creates the grid that shows when page loads
+createGrid();
+
+
+input.addEventListener("change", (e) => {
+    squaresPerRow = e.target.value;
+    totalSquares = squaresPerRow * squaresPerRow;
+    const divs = document.querySelectorAll(".drawing-pad-square")
+
+    // deletes old squares
+    divs.forEach(div => {
+        div.remove();
+    });
+
+    // creates new squares
+    createGrid(squaresPerRow, totalSquares);
+});
+
+
