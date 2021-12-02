975bc1daf0821e77140068e8befca95eed32bfd1
Ganz <loganbellinghamlb@gmail.com>
2021-12-02 18:00:14 +1300

    Experimenting

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // -- ELEMENTS -- //
 const drawingPadContainer = document.querySelector(".drawing-pad-container");
-const input = document.querySelector("input");
+const squaresPerRowInput = document.querySelector("input");
+const rainbowModeButton = document.querySelector(".rainbow-mode-button");
 
 // -- VARIABLES -- //
 let initialSquaresPerRow = 5;
@@ -12,30 +13,56 @@ const createGrid = (squaresPerRow = initialSquaresPerRow, totalSquareAmount = to
     for (let i = 0; i < totalSquareAmount; i++) {
         const div = document.createElement("div");
         div.classList.add("drawing-pad-square");
-       
+
         drawingPadContainer.style.gridTemplateRows = `repeat(${squaresPerRow}, 1fr)`;
         drawingPadContainer.style.gridTemplateColumns = `repeat(${squaresPerRow}, 1fr)`;
-    
+
         drawingPadContainer.appendChild(div);
     }
 };
 
 // creates the grid that shows when page loads
 createGrid();
+let allSquares = document.querySelectorAll(".drawing-pad-square");
+allSquares.forEach(square => {
+    square.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "rgb(30, 30, 30)";
+    })
+});
 
+// remove squares function
+const removeSquares = (squares = allSquares) => {
+    squares.forEach(square => {
+        square.remove();
+    });
+}
 
-input.addEventListener("change", (e) => {
+let rainbowMode = false;
+rainbowModeButton.addEventListener("click", () => {
+    rainbowMode = !rainbowMode;
+    removeSquares();
+    createGrid(squaresPerRow, totalSquares);
+})
+
+squaresPerRowInput.addEventListener("change", (e) => {
     squaresPerRow = e.target.value;
     totalSquares = squaresPerRow * squaresPerRow;
-    const divs = document.querySelectorAll(".drawing-pad-square")
+    const oldSquares = document.querySelectorAll(".drawing-pad-square");
 
     // deletes old squares
-    divs.forEach(div => {
-        div.remove();
-    });
+    removeSquares(oldSquares);
 
     // creates new squares
     createGrid(squaresPerRow, totalSquares);
+    allSquares = document.querySelectorAll(".drawing-pad-square");
+
+    allSquares.forEach(square => {
+        square.addEventListener("mouseenter", (e) => {
+            if (rainbowMode) {
+                e.target.style.backgroundColor = "red";
+            } else {
+                e.target.style.backgroundColor = "rgb(30, 30, 30)";    
+            }
+        });
+    });
 });
-
-
