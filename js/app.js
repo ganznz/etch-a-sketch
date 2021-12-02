d2d9870945d19ba975158d402d323c6ea9d17049
Ganz <loganbellinghamlb@gmail.com>
2021-12-03 01:50:04 +1300

    New code, focusing more on keeping it modular

--- js/app.js
+++ js/app.js
@@ -1,68 +1,105 @@
 // -- ELEMENTS -- //
-const drawingPadContainer = document.querySelector(".drawing-pad-container");
-const squaresPerRowInput = document.querySelector("input");
-const rainbowModeButton = document.querySelector(".rainbow-mode-button");
+const gridSizeInput = document.querySelector(".grid-size-input");
+const generateGridButton = document.querySelector(".generate-grid-button");
+const gridContainer = document.querySelector(".drawing-pad-container");
+const toggleGridButton = document.querySelector(".grid-toggle-button");
+const toggleDrawMethodButton = document.querySelector(".hover-toggle-button");
+const toggleRainbowModeButton = document.querySelector(".rainbow-mode-button");
+const toggleEraserModeButton = document.querySelector(".eraser-mode-button");
+const clearGridButton = document.querySelector(".clear-button");
+
 
 // -- VARIABLES -- //
-let initialSquaresPerRow = 5;
-let totalSquares = initialSquaresPerRow * initialSquaresPerRow;
+drawingMethodVal = true; // hover to draw by default
 
 
 
-const createGrid = (squaresPerRow = initialSquaresPerRow, totalSquareAmount = totalSquares) => {
+let gridSizeInputVal = 5;
+let totalGridSquares = gridSizeInputVal * gridSizeInputVal;
+// get grid size
+const getGridSizeInputVal = e => { 
+    gridSizeInputVal = e.target.value;
+    totalGridSquares = e.target.value * e.target.value;
+}
+gridSizeInput.addEventListener("change", getGridSizeInputVal);
+
+
+const generateGrid = (squaresPerRow, totalSquareAmount) => {
     for (let i = 0; i < totalSquareAmount; i++) {
         const div = document.createElement("div");
-        div.classList.add("drawing-pad-square");
 
-        drawingPadContainer.style.gridTemplateRows = `repeat(${squaresPerRow}, 1fr)`;
-        drawingPadContainer.style.gridTemplateColumns = `repeat(${squaresPerRow}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${squaresPerRow}, 1fr)`;
+        gridContainer.style.gridTemplateColumns = `repeat(${squaresPerRow}, 1fr)`;
 
-        drawingPadContainer.appendChild(div);
-    }
+        gridContainer.appendChild(div);
+    };
 };
 
-// creates the grid that shows when page loads
-createGrid();
-let allSquares = document.querySelectorAll(".drawing-pad-square");
-allSquares.forEach(square => {
-    square.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = "rgb(30, 30, 30)";
-    })
-});
 
-// remove squares function
-const removeSquares = (squares = allSquares) => {
-    squares.forEach(square => {
+const clearGrid = () => {
+    const allGridSquares = document.querySelectorAll(".drawing-pad-container div");
+    allGridSquares.forEach(square => {
         square.remove();
-    });
+    })
+};
+
+// if drawingMethodVal = true
+const hoverDrawingMethod = (e) => {
+    e.target.style.backgroundColor = "red";
 }
 
-let rainbowMode = false;
-rainbowModeButton.addEventListener("click", () => {
-    rainbowMode = !rainbowMode;
-    removeSquares();
-    createGrid(squaresPerRow, totalSquares);
-})
+// if drawingMethodVal = false
+const holdDrawingMethod = (e) => {
+    e.target.style.backgroundColor = "green";
+}
 
-squaresPerRowInput.addEventListener("change", (e) => {
-    squaresPerRow = e.target.value;
-    totalSquares = squaresPerRow * squaresPerRow;
-    const oldSquares = document.querySelectorAll(".drawing-pad-square");
-
-    // deletes old squares
-    removeSquares(oldSquares);
-
-    // creates new squares
-    createGrid(squaresPerRow, totalSquares);
-    allSquares = document.querySelectorAll(".drawing-pad-square");
-
-    allSquares.forEach(square => {
-        square.addEventListener("mouseenter", (e) => {
-            if (rainbowMode) {
-                e.target.style.backgroundColor = "red";
-            } else {
-                e.target.style.backgroundColor = "rgb(30, 30, 30)";    
-            }
+const toggleDrawingMethod = () => {
+    const allGridSquares = document.querySelectorAll(".drawing-pad-container div");
+    if (drawingMethodVal) {
+        allGridSquares.forEach(square => {
+            square.addEventListener("mouseenter", hoverDrawingMethod)
         });
-    });
+    } else {
+        gridContainer.addEventListener("mousedown", () => {
+            console.log("mouse down");
+            allGridSquares.forEach(square => {
+                square.setAttribute("ondragstart", "return false;");
+                square.addEventListener("mouseenter", e => {
+                    e.target.style.backgroundColor = "red";
+                })
+            });
+        })
+        gridContainer.addEventListener("mouseup", () => {
+            console.log("mouse up");
+            allGridSquares.forEach(square => {
+                const bgColor = square.style.backgroundColor;
+                square.addEventListener("mouseenter", e => {
+                    e.target.style.backgroundColor = bgColor;
+                })
+            });
+        })
+    }
+}
+
+
+// changes drawingMethodVal bool value
+toggleDrawMethodButton.addEventListener("click", (e) => {
+    if (drawingMethodVal) {
+        drawingMethodVal = false;
+        e.target.textContent = "Hover to draw";
+    } else {
+        drawingMethodVal = true;
+        e.target.textContent = "Hold to draw";
+    }
+    clearGrid();
+    generateGrid(gridSizeInputVal, totalGridSquares);
+    toggleDrawingMethod();
+});
+
+
+// generates new grid after clicking "Generate" button
+generateGridButton.addEventListener("click", () => {
+    clearGrid();
+    generateGrid(gridSizeInputVal, totalGridSquares);
+    toggleDrawingMethod();
 });
